57d89d0a50642adb3b47d51eec61469696ce5933
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-12-24 04:30:15 +0400

    Create  Notification Model

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,2 +1,3 @@
 import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 import { handleError } from "../utils/error.js";
---
+++ api/models/notification.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    team: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    text: {
+      type: String,
+      required: true,
+    },
+    task: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Task",
+    },
+    notificationType: {
+      type: String,
+      default: "alert",
+      enum: ["alert", "message"],
+    },
+    isRead: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+export default Notification;
